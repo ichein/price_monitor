cd24b8cd937021964df9c594c36c627b53c7778f
aleja <alejandro13arranaga@gmail.com>
2026-09-16 23:09:19 -0600

    Refactor popup dialog layout and style for improved user experience

--- core/notifier.py
+++ core/notifier.py
@@ -1,6 +1,6 @@
 #Nota: Envía avisos por consola, Telegram, correo o ventana emergente.
 import sys
-from  PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QDialog, QLabel
+from PyQt6.QtWidgets import QApplication, QPushButton, QHBoxLayout, QVBoxLayout, QDialog, QLabel
 from PyQt6.QtCore import Qt
 
 mensaje = {
@@ -20,28 +20,41 @@ telegram_config = {
 }
 
 class popup(QDialog):
-    def __init__(self):
-        super().__init__()
+    def centrar_en_padre(self, parent):
+        geo_padre = parent.Geometry()
+        x = geo_padre.x() + (geo_padre.width() - self.width()) // 2
+        y = geo_padre.y() + (geo_padre.height() - self.height()) // 2
+        self.move(x, y)
+    #----------------------
+    def __init__(self, parent=None):
+        super().__init__(parent)
         self.setWindowTitle(mensaje["titulo"])
         self.setGeometry(100, 100, 100, 100)
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel(mensaje["mensaje"]))
-        boton=QPushButton("X")
+        main_layout = QHBoxLayout(self)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        label = QLabel(mensaje["mensaje"])
+        label.setMinimumHeight(100)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        boton = QPushButton("X")
         boton.clicked.connect(self.close)
-        boton.setGeometry(50, 50, 50, 30)
-        layout.addWidget(boton)
-        self.setLayout(layout)
+        boton.setMinimumHeight(100)
+        main_layout.addWidget(label)
+        main_layout.addWidget(boton)
+        self.setLayout(main_layout)
         self.setFixedSize(250, 100)
+        if parent:
+            self.centrar_en_padre(parent)
         self.setWindowFlags(
             Qt.WindowType.Window
             | Qt.WindowType.FramelessWindowHint
-            | Qt.WindowType.WindowTitleHint)
+            | Qt.WindowType.WindowTitleHint
+        )
 
 app = QApplication(sys.argv)
-with open("../style/popup.qss", "r") as archivo:
+with open("style/popup.qss", "r") as archivo:
     app.setStyleSheet(archivo.read())
 
 #prueva de pop up
-ventana = popup()
+ventana = popup(self)
 ventana.show()
 sys.exit(app.exec())
